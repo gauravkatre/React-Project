32805526bddd6be36de5b6bd605c268c340f6b10
Gaurav Katre <gauravkatre@gmail.com>
2026-02-16 23:26:56 +0530

    todo context api finished

---
+++ Todo_context/src/context/TodoContext.js
@@ -0,0 +1,22 @@
+import { useContext,createContext } from "react";
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            title:"Learn React",
+            completed:false
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+    
+});
+
+export const TodoContextProvider =TodoContext.Provider;
+
+export const useTodo = () =>{
+    return useContext(TodoContext);
+}
